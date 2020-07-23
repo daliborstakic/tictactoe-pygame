e81ebc06258c15c1385f5193327ecdd89933f71c
daliborstakic <stakicdalibor64@gmail.com>
2020-07-23 09:42:38 +0200

    Added the draw_grid() function.

--- main.py
+++ main.py
@@ -4,17 +4,36 @@ pygame.init()
 
 # Screen
 WIDTH = 300
+ROWS = 3
 win = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("TicTacToe")
 
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
 
+def draw_grid():
+    gap = WIDTH // ROWS
+
+    # Starting points
+    x = 0
+    y = 0
+
+    for i in range(ROWS):
+        x = i * gap
+        y = i * gap
+
+        pygame.draw.line(win, GRAY, (x, 0), (x, WIDTH), 3)
+        pygame.draw.line(win, GRAY, (0, y), (WIDTH, y), 3)
+
+
 def render():
     win.fill(WHITE)
+    draw_grid()
     pygame.display.update()
 
 
