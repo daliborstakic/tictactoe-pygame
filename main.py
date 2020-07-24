5293c65ddb9f41d8285070d30b07f07826eeeb75
daliborstakic <stakicdalibor64@gmail.com>
2020-07-24 15:40:30 +0200

    Added winning conditions for both players.

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ BLUE = (0, 0, 255)
 X_IMAGE = pygame.transform.scale(pygame.image.load("images/x.png"), (80, 80))
 O_IMAGE = pygame.transform.scale(pygame.image.load("images/o.png"), (80, 80))
 
+# Fonts
+END_FONT = pygame.font.SysFont('arial', 40)
+
 
 def draw_grid():
     gap = WIDTH // ROWS
@@ -39,6 +42,7 @@ def draw_grid():
 def initialize_grid():
     dis_to_cen = WIDTH // ROWS // 2
 
+    # Initializing the array
     game_array = [[None, None, None], [None, None, None], [None, None, None]]
 
     for i in range(len(game_array)):
@@ -46,6 +50,7 @@ def initialize_grid():
             x = dis_to_cen * (2 * j + 1)
             y = dis_to_cen * (2 * i + 1)
 
+            # Adding centre coordinates
             game_array[i][j] = (x, y, "", True)
 
     return game_array
@@ -54,22 +59,25 @@ def initialize_grid():
 def click(game_array):
     global x_turn, o_turn, images
 
+    # Mouse position
     m_x, m_y = pygame.mouse.get_pos()
 
     for i in range(len(game_array)):
         for j in range(len(game_array[i])):
             x, y, char, can_play = game_array[i][j]
 
+            # Distance between mouse and the centre of the square
             dis = math.sqrt((x - m_x) ** 2 + (y - m_y) ** 2)
 
-            if dis < WIDTH // ROWS // 2:
-                if x_turn and can_play:
+            # If it's inside the square
+            if dis < WIDTH // ROWS // 2 and can_play:
+                if x_turn:  # If it's X's turn
                     images.append((x, y, X_IMAGE))
                     x_turn = False
                     o_turn = True
                     game_array[i][j] = (x, y, 'x', False)
 
-                elif o_turn and can_play:
+                elif o_turn:  # If it's O's turn
                     images.append((x, y, O_IMAGE))
                     x_turn = True
                     o_turn = False
@@ -77,12 +85,45 @@ def click(game_array):
 
 
 def has_won(game_array):
-    pass
+    # Checking rows
+    for row in range(len(game_array)):
+        if (game_array[row][0][2] == game_array[row][1][2] == game_array[row][2][2]) and game_array[row][0][2] != "":
+            display_message(game_array[row][0][2].upper() + " has won!")
+            return True
+
+    # Checking columns
+    for col in range(len(game_array)):
+        if (game_array[0][col][2] == game_array[1][col][2] == game_array[2][col][2]) and game_array[0][col][2] != "":
+            display_message(game_array[0][col][2].upper() + " has won!")
+            return True
+
+    # Checking main diagonal
+    if (game_array[0][0][2] == game_array[1][1][2] == game_array[2][2][2]) and game_array[0][0][2] != "":
+        display_message(game_array[0][0][2].upper() + " has won!")
+        return True
+
+    # Checking reverse diagonal
+    if (game_array[0][2][2] == game_array[1][1][2] == game_array[2][0][2]) and game_array[0][2][2] != "":
+        display_message(game_array[0][2][2].upper() + " has won!")
+        return True
+
+    return False
+
+
+def display_message(content):
+    pygame.time.delay(500)
+    win.fill(WHITE)
+    end_text = END_FONT.render(content, 1, BLACK)
+    win.blit(end_text, ((WIDTH - end_text.get_width()) // 2, (WIDTH - end_text.get_height()) // 2))
+    pygame.display.update()
+    pygame.time.delay(3000)
 
 
 def render():
     win.fill(WHITE)
     draw_grid()
+
+    # Drawing X's and O's
     for image in images:
         x, y, IMAGE = image
         win.blit(IMAGE, (x - IMAGE.get_width() // 2, y - IMAGE.get_height() // 2))
@@ -102,7 +143,7 @@ def main():
 
     game_array = initialize_grid()
 
-    while True:
+    while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -111,6 +152,10 @@ def main():
 
         render()
 
+        if has_won(game_array):
+            run = False
 
-if __name__ == '__main__':
-    main()
+
+while True:
+    if __name__ == '__main__':
+        main()
